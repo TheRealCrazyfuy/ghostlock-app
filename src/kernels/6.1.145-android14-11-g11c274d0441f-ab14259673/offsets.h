a9c593809c2f3930066a3e7f6017610cd75b055f
TheRealCrazyfuy <108433136+TheRealCrazyfuy@users.noreply.github.com>
2026-08-20 13:25:55 +0200

    feat: wip add another kernel for redmagic 9

---
+++ src/kernels/6.1.145-android14-11-g11c274d0441f-ab14259673/offsets.h
@@ -0,0 +1,23 @@
+/* 6.1.145-android14-11-g11c274d0441f-ab14259673 */
+
+OFFSETS_ENTRY(
+"6.1.145-android14-11-g11c274d0441f-ab14259673",
+STRUCT_OFFSETS_6_1,
+.pselect_waiter_shift = -2,
+.off_init_task = 0x0a00f600,
+.off_init_cred = 0x0a021a68,
+.off_root_task_group = 0x0a1f7580,
+.off_selinux_enforcing = 0x09fba8b8,
+.off_selinux_blob_sizes = 0x095cb248,
+.off_security_hook_heads = 0x095cab38,
+.off_slide_nfulnl_logger = 0x0a0029c8,
+.off_slide_boot_id = 0x0a1a1b00, // wip
+.off_slide_loggers_0_1 = 0x09fb2910, // wip
+),
+
+/* BTF reference (runtime uses target.h defaults): */
+/* #define STRUCT_PAGE_SIZE 0x40 */
+/* #define STRUCT_PAGE_COMPOUND_HEAD 0x8 */
+/* #define STRUCT_PAGE_TYPE 0x30 */
+/* #define STRUCT_SLAB_CACHE 0x8 */
+/* #define STRUCT_MM_STRUCT 0x4C0 */
