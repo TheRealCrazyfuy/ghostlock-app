7e03c3464d9b820dfe01d38c8e33184a9e28a89b
TheRealCrazyfuy <108433136+TheRealCrazyfuy@users.noreply.github.com>
2026-08-28 22:08:10 +0200

    fix: kernel offsets

--- src/kernels/6.1.145-android14-11-g11c274d0441f-ab14259673/offsets.h
+++ src/kernels/6.1.145-android14-11-g11c274d0441f-ab14259673/offsets.h
@@ -1,23 +1,23 @@
 /* 6.1.145-android14-11-g11c274d0441f-ab14259673 */
 
 OFFSETS_ENTRY(
-"6.1.145-android14-11-g11c274d0441f-ab14259673",
-STRUCT_OFFSETS_6_1,
-.pselect_waiter_shift = -2,
-.off_init_task = 0x0a00f600,
-.off_init_cred = 0x0a021a68,
-.off_root_task_group = 0x0a1f7580,
-.off_selinux_enforcing = 0x09fba8b8,
-.off_selinux_blob_sizes = 0x095cb248,
-.off_security_hook_heads = 0x095cab38,
-.off_slide_nfulnl_logger = 0x0a0029c8,
-.off_slide_boot_id = 0x0a26a498,
-.off_slide_loggers_0_1 = 0x09fb2910, // wip
+    "6.1.145-android14-11-g11c274d0441f-ab14259673",
+    STRUCT_OFFSETS_6_1,
+    .pselect_waiter_shift = 1,
+    .off_init_task = 0x0200f600,
+    .off_init_cred = 0x02021a68,
+    .off_root_task_group = 0x021f7580,
+    .off_selinux_enforcing = 0x02249400,
+    .off_selinux_blob_sizes = 0x015cb248,
+    .off_security_hook_heads = 0x015cab38,
+    .off_slide_nfulnl_logger = 0x020029c8,
+    .off_slide_boot_id = 0x0226a498,
+    .off_slide_loggers_0_1 = 0x02002918,
 ),
 
 /* BTF reference (runtime uses target.h defaults): */
 /* #define STRUCT_PAGE_SIZE 0x40 */
 /* #define STRUCT_PAGE_COMPOUND_HEAD 0x8 */
 /* #define STRUCT_PAGE_TYPE 0x30 */
-/* #define STRUCT_SLAB_CACHE 0x8 */
-/* #define STRUCT_MM_STRUCT 0x4C0 */
+/* #define STRUCT_SLAB_CACHE 0x18 */
+/* #define STRUCT_MM_STRUCT 0x3C0 */
--- src/kernels/6.1.90-android14-11-g6f645aac9706-ab12424481/offsets.h
+++ src/kernels/6.1.90-android14-11-g6f645aac9706-ab12424481/offsets.h
@@ -1,23 +1,23 @@
 /* 6.1.90-android14-11-g6f645aac9706-ab12424481 */
 
 OFFSETS_ENTRY(
-"6.1.90-android14-11-g6f645aac9706-ab12424481",
-STRUCT_OFFSETS_6_1,
-.pselect_waiter_shift = -2,
-.off_init_task = 0x09fbed80,
-.off_init_cred = 0x09fd12b0,
-.off_root_task_group = 0x0a1a4580,
-.off_selinux_enforcing = 0x09f6a8a0,
-.off_selinux_blob_sizes = 0x0959ae88,
-.off_security_hook_heads = 0x0959a778,
-.off_slide_nfulnl_logger = 0x09fb29c8,
-.off_slide_boot_id = 0x0a1a1b00,
-.off_slide_loggers_0_1 = 0x09fb2910,
+    "6.1.90-android14-11-g6f645aac9706-ab12424481",
+    STRUCT_OFFSETS_6_1,
+    .pselect_waiter_shift = 1,
+    .off_init_task = 0x01fbed80,
+    .off_init_cred = 0x01fd12b0,
+    .off_root_task_group = 0x021a4580,
+    .off_selinux_enforcing = 0x021f63f0,
+    .off_selinux_blob_sizes = 0x0159ae88,
+    .off_security_hook_heads = 0x0159a778,
+    .off_slide_nfulnl_logger = 0x01fb29c8,
+    .off_slide_boot_id = 0x02217480,
+    .off_slide_loggers_0_1 = 0x01fb2918,
 ),
 
 /* BTF reference (runtime uses target.h defaults): */
 /* #define STRUCT_PAGE_SIZE 0x40 */
 /* #define STRUCT_PAGE_COMPOUND_HEAD 0x8 */
 /* #define STRUCT_PAGE_TYPE 0x30 */
-/* #define STRUCT_SLAB_CACHE 0x8 */
-/* #define STRUCT_MM_STRUCT 0x4C0 */
+/* #define STRUCT_SLAB_CACHE 0x18 */
+/* #define STRUCT_MM_STRUCT 0x3C0 */
